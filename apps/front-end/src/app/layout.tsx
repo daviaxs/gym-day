8ced37c227db35130fac301e4a5db57b739ca197
Davi Alves <cadwvk@gmail.com>
2023-11-22 16:33:22 -0300

    setup: moved fonts for new file

--- apps/front-end/src/app/layout.tsx
+++ apps/front-end/src/app/layout.tsx
@@ -1,18 +1,6 @@
+import { roboto } from '@/shared/theme/fonts'
 import type { Metadata } from 'next'
-import { Inter, Roboto } from 'next/font/google'
 import './Global.css'
-
-export const inter = Inter({ 
-  weight: ['400', '700', '800'],
-  subsets: ['latin'],
-  preload: true,
-})
-
-export const roboto = Roboto({ 
-  weight: ['500', '700', '900'],
-  subsets: ['latin'],
-  preload: true,
-})
 
 export const metadata: Metadata = {
   title: 'Gym Day',

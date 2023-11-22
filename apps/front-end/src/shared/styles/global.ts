4c9d36288a231e770b6d7404ee7b56625667fc93
Davi Alves <cadwvk@gmail.com>
2023-11-22 17:24:54 -0300

    style: updated bg color and texts color

--- apps/front-end/src/shared/styles/global.ts
+++ apps/front-end/src/shared/styles/global.ts
@@ -1,6 +1,7 @@
 'use client'
 
 import { createGlobalStyle } from "styled-components";
+import { themeColors } from "./theme/palette";
 
 export const GlobalStyle = createGlobalStyle`
   * {
@@ -9,5 +10,10 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+  }
+
+  body {
+    background-color: ${themeColors["blue-dark-900"]};
+    color: ${themeColors['grey-50']};
   }
 `

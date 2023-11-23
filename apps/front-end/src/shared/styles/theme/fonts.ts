bb83f4b260cded9c49e8d82debd556b4a087addf
Davi Alves <cadwvk@gmail.com>
2023-11-23 19:23:50 -0300

    fix: eslint updated fonts file formatting

--- apps/front-end/src/shared/styles/theme/fonts.ts
+++ apps/front-end/src/shared/styles/theme/fonts.ts
@@ -1,12 +1,12 @@
-import { Inter, Roboto } from "next/font/google"
+import { Inter, Roboto } from 'next/font/google'
 
-export const inter = Inter({ 
+export const inter = Inter({
   weight: ['400', '700', '800'],
   subsets: ['latin'],
   preload: true,
 })
 
-export const roboto = Roboto({ 
+export const roboto = Roboto({
   weight: ['500', '700', '900'],
   subsets: ['latin'],
   preload: true,

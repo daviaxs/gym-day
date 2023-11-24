87d175aa882ae796f449576815832a681eed71dc
Davi Alves <cadwvk@gmail.com>
2023-11-24 17:28:35 -0300

    feat: added prop size in Text component

--- apps/front-end/src/app/(home)/page.tsx
+++ apps/front-end/src/app/(home)/page.tsx
@@ -3,7 +3,7 @@ import { Text } from '@/shared/components/text/Text'
 export default function Home() {
   return (
     <div>
-      <Text as="p" color="orange-400">
+      <Text as="p" color="orange-400" size="lg">
         Gym Day - Hello World
       </Text>
     </div>
--- apps/front-end/src/shared/components/text/Text.ts
+++ apps/front-end/src/shared/components/text/Text.ts
@@ -1,13 +1,16 @@
 'use client'
 
 import { themeColors } from '@/shared/styles/theme/palette'
+import { themeSizes } from '@/shared/styles/theme/sizes'
 import styled from 'styled-components'
 
 interface TextProps {
   as: keyof JSX.IntrinsicElements
   color: keyof typeof themeColors
+  size: keyof typeof themeSizes
 }
 
 export const Text = styled.p<TextProps>`
   color: ${({ color }) => themeColors[color]};
+  font-size: ${({ size }) => themeSizes[size]}rem;
 `

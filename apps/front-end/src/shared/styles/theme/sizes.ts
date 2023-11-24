8ed48bb7488b50325a91e41bb7587b204454da59
Davi Alves <cadwvk@gmail.com>
2023-11-24 17:26:45 -0300

    fix: updated PXs to REM in themeSizes

--- apps/front-end/src/shared/styles/theme/sizes.ts
+++ apps/front-end/src/shared/styles/theme/sizes.ts
@@ -1,13 +1,14 @@
+/* eslint-disable prettier/prettier */
 export const themeSizes = {
-  xs: 4,
-  sm: 8,
-  md: 16,
-  lgSm: 20,
-  lg: 24,
-  xlSm: 26,
-  xl: 32,
-  xxl: 36,
-  '2xl': 48,
-  '3xl': 64,
-  '4xl': 96,
+  xs: 0.25,    // 4px
+  sm: 0.5,     // 8px
+  md: 1,       // 16px
+  lgSm: 1.25,  // 20px
+  lg: 1.5,     // 24px
+  xlSm: 1.625, // 26px
+  xl: 2,       // 32px
+  xxl: 2.25,   // 36px
+  '2xl': 3,    // 48px
+  '3xl': 4,    // 64px
+  '4xl': 6,    // 96px
 } as const

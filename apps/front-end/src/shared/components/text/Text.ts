e9c8d8f52a14282229b5264ad1fc9b207595fc3c
Davi Alves <cadwvk@gmail.com>
2023-12-06 15:34:25 -0300

    feat: added weight prop in Text component

--- apps/front-end/src/shared/components/text/Text.ts
+++ apps/front-end/src/shared/components/text/Text.ts
@@ -8,9 +8,12 @@ interface TextProps {
   as: keyof JSX.IntrinsicElements
   color: keyof typeof themeColors
   size: keyof typeof themeSizes
+  weight?: '400' | '500' | '700' | '800' | '900'
 }
 
 export const Text = styled.p<TextProps>`
   color: ${({ color }) => themeColors[color]};
   font-size: ${({ size }) => themeSizes[size]}rem;
+  font-weight: ${({ weight }) => weight};
+  line-height: 90%;
 `

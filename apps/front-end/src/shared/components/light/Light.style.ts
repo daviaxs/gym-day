15eae40305a63c58b199ad7935a680ea107762e9
Davi Alves <cadwvk@gmail.com>
2023-12-06 16:27:35 -0300

    feat: added Light component

---
+++ apps/front-end/src/shared/components/light/Light.style.ts
@@ -0,0 +1,29 @@
+'use client'
+
+import { themeColors } from '@/shared/styles/theme/palette'
+import styled from 'styled-components'
+
+export interface LightProps {
+  intensity?: 'high' | 'medium' | 'low'
+  color: 'blue-500' | 'blue-300'
+}
+
+export const LightStyle = styled.span<LightProps>`
+  width: 30rem;
+  height: 30rem;
+  border-radius: 100%;
+  filter: blur(150px);
+  z-index: -1;
+
+  opacity: ${(props) =>
+    props.intensity === 'high'
+      ? '100%'
+      : props.intensity === 'medium'
+        ? '50%'
+        : '30%'};
+
+  background-color: ${(props) =>
+    props.color === 'blue-500'
+      ? themeColors['blue-500']
+      : themeColors['blue-300']};
+`

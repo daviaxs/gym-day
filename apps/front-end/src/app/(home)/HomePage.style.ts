e042400f2efd4136994ce6f024c3ff3414416385
Davi Alves <cadwvk@gmail.com>
2023-12-10 15:50:47 -0300

    feat: added IntroductionBG in home page

---
+++ apps/front-end/src/app/(home)/HomePage.style.ts
@@ -0,0 +1,44 @@
+'use client'
+
+import styled from 'styled-components'
+
+export const HomePageContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+`
+
+export const IntroductionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
+
+  position: relative;
+  overflow: hidden;
+
+  width: 100%;
+  height: 950px;
+`
+
+export const IntroductionBG = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  overflow: hidden;
+
+  display: flex;
+  align-items: end;
+  justify-content: center;
+
+  .lines {
+    width: 100%;
+    height: 100%;
+  }
+
+  .light {
+    position: absolute;
+    width: 60%;
+    bottom: -25rem;
+  }
+`
--- apps/front-end/src/app/(home)/page.tsx
+++ apps/front-end/src/app/(home)/page.tsx
@@ -1,9 +1,29 @@
 import { Header } from './utils/header/Header'
+import {
+  HomePageContainer,
+  IntroductionBG,
+  IntroductionContainer,
+} from './HomePage.style'
+import { Light } from '@/shared/components/light/Light'
+import Image from 'next/image'
+import homePageLine from '@/shared/assets/imgs/home-page-line.png'
+import { Text } from '@/shared/components/text/Text'
 
 export default function Home() {
   return (
-    <div>
+    <HomePageContainer>
       <Header />
-    </div>
+
+      <IntroductionContainer>
+        <Text color="grey-50" size="2xl">
+          Gym Day
+        </Text>
+
+        <IntroductionBG>
+          <Image src={homePageLine} className="lines" alt="" priority />
+          <Light color="blue-300" />
+        </IntroductionBG>
+      </IntroductionContainer>
+    </HomePageContainer>
   )
 }

927a434d616d0525f745839050c7bd71d1b7d989
Davi Alves <cadwvk@gmail.com>
2023-12-10 16:24:58 -0300

    css: updated scrollbar

--- apps/front-end/src/shared/styles/global.ts
+++ apps/front-end/src/shared/styles/global.ts
@@ -21,4 +21,29 @@ export const GlobalStyle = createGlobalStyle`
     background-color: ${themeColors['blue-dark-900']};
     color: ${themeColors['grey-50']};
   }
+
+  /* ===== Scrollbar CSS ===== */
+  /* Firefox */
+  * {
+    scrollbar-width: auto;
+    scrollbar-color: #8a8ea1 #151928;
+  }
+
+  ::-webkit-scrollbar-corner {
+    background-color: #151928;
+  }
+
+  /* Chrome, Edge, and Safari */
+  *::-webkit-scrollbar {
+    width: 5px;
+  }
+
+  *::-webkit-scrollbar-track {
+    background: #151928;
+  }
+
+  *::-webkit-scrollbar-thumb {
+    background-color: #8a8ea1;
+    border-radius: 10px;
+  }
 `

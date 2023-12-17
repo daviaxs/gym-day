212c7b5a406e63958262ccd7b9c01a35e6a77ae0
Davi Alves <cadwvk@gmail.com>
2023-12-17 14:33:49 -0300

    feat: Add new SVG icons

---
+++ apps/front-end/src/shared/assets/icons/DumbbellGradiant.tsx
@@ -0,0 +1,32 @@
+export function DumbbellGradiant() {
+  return (
+    <svg
+      width="73"
+      height="73"
+      viewBox="0 0 73 73"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M18.625 18.625L54.375 54.375M65.75 65.75L62.5 62.5M7.25 7.25L10.5 10.5M56 69L69 56M4 17L17 4M7.25 30L30 7.25M43 65.75L65.75 43"
+        stroke="url(#paint0_linear_85_148)"
+        strokeWidth="8"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_85_148"
+          x1="76.54"
+          y1="-1.46"
+          x2="17.26"
+          y2="69.26"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#FF7A00" />
+          <stop offset="1" stopColor="#0500FF" />
+        </linearGradient>
+      </defs>
+    </svg>
+  )
+}

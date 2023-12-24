20043e959e6cc8e5c8b02fc884d9d38344162ca3
Davi Alves <cadwvk@gmail.com>
2023-12-24 15:55:41 -0300

    feat: added Form component in SignUp page

--- apps/front-end/src/app/auth/registrar/page.tsx
+++ apps/front-end/src/app/auth/registrar/page.tsx
@@ -1,7 +1,11 @@
 import { AuthContainer } from '../AuthPage.style'
-import { Form } from '../utils/form/Form'
 import { Light } from '@/shared/components/light/Light'
 import { Welcome } from '../utils/welcome'
+import { Form } from '../utils/form'
+import { Button } from '@/shared/components/button/Button'
+import { Text } from '@/shared/components/text/Text'
+import { TextSpan } from '@/shared/components/text/TextSpan'
+import { SubmitButton } from '../utils/form/Form.style'
 
 export default function SignUp() {
   return (
@@ -17,7 +21,31 @@ export default function SignUp() {
         </Welcome.Description>
       </Welcome.Container>
 
-      <Form title="Criar conta" />
+      <Form.Container title="Criar conta">
+        <Form.Content>
+          <Form.Input name="email" type="email" placeholder="Email" />
+          <Form.Input name="name" type="text" placeholder="Nome" />
+
+          <Form.Input name="password" type="password" placeholder="Senha" />
+          <Form.Input
+            name="verifyPassword"
+            type="password"
+            placeholder="Confirmar senha"
+          />
+
+          <SubmitButton>
+            <Text size="lgSm" $textalign="center" $lineheight="110%">
+              Ao se registrar, você aceita nossos{' '}
+              <TextSpan hoverUnderline>termos de uso</TextSpan> e a nossa
+              <TextSpan hoverUnderline> política de privacidade</TextSpan>.
+            </Text>
+
+            <Button type="submit" size="full">
+              Criar conta
+            </Button>
+          </SubmitButton>
+        </Form.Content>
+      </Form.Container>
 
       <Light color="grey-500" />
     </AuthContainer>

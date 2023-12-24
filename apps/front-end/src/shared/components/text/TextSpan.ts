16a5a6466c0024d8b4d491962a42939c183300c9
Davi Alves <cadwvk@gmail.com>
2023-12-24 15:53:35 -0300

    feat: Add TextSpan component

---
+++ apps/front-end/src/shared/components/text/TextSpan.ts
@@ -0,0 +1,19 @@
+'use client'
+
+import { themeColors } from '@/shared/styles/theme/palette'
+import styled from 'styled-components'
+
+interface TextSpanProps {
+  color?: keyof typeof themeColors
+  hoverUnderline?: boolean
+}
+
+export const TextSpan = styled.span<TextSpanProps>`
+  color: ${({ color }) =>
+    themeColors[color as keyof typeof themeColors] || themeColors['blue-400']};
+
+  &:hover {
+    ${({ hoverUnderline }) =>
+      hoverUnderline && `text-decoration: underline; cursor: pointer;`}
+  }
+`

5212d4a832ae46206c0d3f2f0b5edcdb79d0ff64
Davi Alves <cadwvk@gmail.com>
2023-12-27 16:11:46 -0300

    feat: Add useSignUpForm hook in SignUp page

--- apps/front-end/src/app/auth/registrar/page.tsx
+++ apps/front-end/src/app/auth/registrar/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { AuthContainer } from '../AuthPage.style'
 import { Light } from '@/shared/components/light/Light'
 import { Welcome } from '../utils/welcome'
@@ -6,8 +8,11 @@ import { Button } from '@/shared/components/button/Button'
 import { Text } from '@/shared/components/text/Text'
 import { TextSpan } from '@/shared/components/text/TextSpan'
 import { SubmitButton } from '../utils/form/Form.style'
+import { useSignUpForm } from '@/shared/hooks/useSignUpForm'
 
 export default function SignUp() {
+  const { buttonDisabled, handleInputChange, handleSubmit } = useSignUpForm()
+
   return (
     <AuthContainer>
       <Welcome.Container backLink>
@@ -22,25 +27,45 @@ export default function SignUp() {
       </Welcome.Container>
 
       <Form.Container title="Criar conta">
-        <Form.Content>
-          <Form.Input name="email" type="email" placeholder="Email" />
-          <Form.Input name="name" type="text" placeholder="Nome" />
+        <Form.Content onSubmit={handleSubmit}>
+          <Form.Input
+            name="email"
+            type="email"
+            placeholder="Email"
+            onChange={handleInputChange}
+          />
+          <Form.Input
+            name="name"
+            type="text"
+            placeholder="Nome"
+            autoComplete="name"
+            onChange={handleInputChange}
+          />
 
-          <Form.Input name="password" type="password" placeholder="Senha" />
+          <Form.Input
+            name="password"
+            type="password"
+            placeholder="Senha"
+            autoComplete="new-password"
+            onChange={handleInputChange}
+          />
+
           <Form.Input
             name="verifyPassword"
             type="password"
             placeholder="Confirmar senha"
+            autoComplete="new-password"
+            onChange={handleInputChange}
           />
 
           <SubmitButton>
             <Text size="lgSm" $textalign="center" $lineheight="110%">
               Ao se registrar, você aceita nossos{' '}
-              <TextSpan hoverUnderline>termos de uso</TextSpan> e a nossa
-              <TextSpan hoverUnderline> política de privacidade</TextSpan>.
+              <TextSpan $hoverUnderline>termos de uso</TextSpan> e a nossa
+              <TextSpan $hoverUnderline> política de privacidade</TextSpan>.
             </Text>
 
-            <Button type="submit" size="full">
+            <Button type="submit" size="full" disabled={buttonDisabled}>
               Criar conta
             </Button>
           </SubmitButton>

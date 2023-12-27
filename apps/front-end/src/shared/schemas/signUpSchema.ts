ab9697449a203453ac1e1753bbf582898d61f0e4
Davi Alves <cadwvk@gmail.com>
2023-12-27 20:05:34 -0300

    ref: Refactor signUpSchema in front-end app

--- apps/front-end/src/shared/schemas/signUpSchema.ts
+++ apps/front-end/src/shared/schemas/signUpSchema.ts
@@ -2,12 +2,32 @@ import { z } from 'zod'
 
 export const signUpSchema = z
   .object({
-    name: z.string().min(3),
-    email: z.string().email(),
-    password: z.string().min(6),
-    verifyPassword: z.string().min(6),
+    name: z
+      .string()
+      .min(3, { message: 'O nome precisa ter no mínimo 3 caracteres' })
+      .transform((name) => {
+        return name
+          .trim()
+          .split(' ')
+          .map((word) => {
+            return word[0]
+              .toLocaleUpperCase()
+              .concat(word.substring(1).toLocaleLowerCase())
+          })
+          .join(' ')
+      }),
+
+    email: z.string().email('E-mail inválido'),
+
+    password: z.string().min(6, 'A senha precisa de no mínimo 6 caracteres'),
+
+    verifyPassword: z
+      .string()
+      .min(6, 'A senha precisa de no mínimo 6 caracteres'),
   })
   .refine((data) => data.password === data.verifyPassword, {
     message: 'As senhas devem ser iguais',
     path: ['verifyPassword'],
   })
+
+export type createUserData = z.infer<typeof signUpSchema>

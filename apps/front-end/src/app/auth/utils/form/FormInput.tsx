bd817d748c442da198a90110bc0ccb07dbfe3101
Davi Alves <cadwvk@gmail.com>
2023-12-27 20:05:10 -0300

    feat: Update FormInput component

--- apps/front-end/src/app/auth/utils/form/FormInput.tsx
+++ apps/front-end/src/app/auth/utils/form/FormInput.tsx
@@ -7,16 +7,19 @@ import { ShowPasswordIcons } from './utils/ShowPasswordIcons'
 import { useShowPassword } from '@/shared/hooks/useShowPassword'
 
 interface FormInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
-  name: string
+  name?: string
   type: InputType
   icon?: ReactNode
   placeholder?: string
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  register?: any
 }
 
 export function FormInput({
   name,
   type,
   placeholder,
+  register,
   ...props
 }: FormInputProps) {
   const { toggleShowPassword, verifyInputType, showPassword } = useShowPassword(
@@ -31,6 +34,7 @@ export function FormInput({
         type={verifyInputType}
         name={name}
         placeholder={placeholder}
+        {...register}
         {...props}
       />
 

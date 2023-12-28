139cc11bbae7339c36eb79fdbf85e46ebc56ebaf
Davi Alves <cadwvk@gmail.com>
2023-12-28 16:09:04 -0300

    feat: Refactor sign-in form and add form validation

--- apps/front-end/src/app/auth/entrar/page.tsx
+++ apps/front-end/src/app/auth/entrar/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { Light } from '@/shared/components/light/Light'
 import { AuthContainer } from '../AuthPage.style'
 import { Welcome } from '../utils/welcome'
@@ -7,8 +9,13 @@ import { SpanContainer, SubmitButton } from '../utils/form/Form.style'
 import { Text } from '@/shared/components/text/Text'
 import { TextSpan } from '@/shared/components/text/TextSpan'
 import Link from 'next/link'
+import { FormProvider } from 'react-hook-form'
+import { useSignInForm } from '@/shared/hooks/useSignInForm'
+import { ErroMessage } from '../utils/form/ErrorMenssage'
 
 export default function SignIn() {
+  const { methods, signIn } = useSignInForm()
+
   return (
     <AuthContainer>
       <Welcome.Container>
@@ -17,39 +24,51 @@ export default function SignIn() {
         </Welcome.Title>
       </Welcome.Container>
 
-      <Form.Container title="Criar conta">
-        <Form.Content>
-          <Form.Input
-            name="email"
-            type="email"
-            placeholder="Email"
-            autoComplete="current-email"
-          />
+      <FormProvider {...methods}>
+        <Form.Container title="Criar conta">
+          <Form.Content onSubmit={methods.handleSubmit(signIn)}>
+            <SpanContainer>
+              <Form.Input
+                type="email"
+                placeholder="Email"
+                autoComplete="current-email"
+                register={methods.register('email')}
+              />
+              <ErroMessage name="email" />
+            </SpanContainer>
 
-          <SpanContainer className="passwordSpanContainer">
-            <Form.Input
-              name="password"
-              type="password"
-              placeholder="Senha"
-              autoComplete="current-password"
-            />
-            <TextSpan color="blue-400">Esqueci minha senha</TextSpan>
-          </SpanContainer>
+            <SpanContainer className="passwordSpanContainer">
+              <Form.Input
+                type="password"
+                placeholder="Senha"
+                autoComplete="current-password"
+                register={methods.register('password')}
+              />
+              <ErroMessage name="password" />
+              <TextSpan color="blue-400">Esqueci minha senha</TextSpan>
+            </SpanContainer>
 
-          <SubmitButton>
-            <Button type="submit" size="full">
-              Entrar
-            </Button>
+            <SubmitButton>
+              <Button
+                type="submit"
+                size="full"
+                disabled={
+                  !methods.formState.isValid || methods.formState.isSubmitting
+                }
+              >
+                Entrar
+              </Button>
 
-            <Text size="lgSm" $textalign="center" $lineheight="110%">
-              Não tem uma conta?{' '}
-              <Link href="/auth/registrar">
-                <TextSpan $hoverUnderline>Registre-se</TextSpan>
-              </Link>
-            </Text>
-          </SubmitButton>
-        </Form.Content>
-      </Form.Container>
+              <Text size="lgSm" $textalign="center" $lineheight="110%">
+                Não tem uma conta?{' '}
+                <Link href="/auth/registrar">
+                  <TextSpan $hoverUnderline>Registre-se</TextSpan>
+                </Link>
+              </Text>
+            </SubmitButton>
+          </Form.Content>
+        </Form.Container>
+      </FormProvider>
 
       <Light color="grey-500" />
     </AuthContainer>

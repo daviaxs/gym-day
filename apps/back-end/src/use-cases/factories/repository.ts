2bffad9e787872154f9a2219598935976cec562c
Davi Alves <cadwvk@gmail.com>
2023-12-30 16:00:32 -0300

    feat: Update usersRepository instantiation based on environment

--- apps/back-end/src/use-cases/factories/repository.ts
+++ apps/back-end/src/use-cases/factories/repository.ts
@@ -1,4 +1,8 @@
+import { env } from '@/env'
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
 
-export const usersRepository = new PrismaUsersRepository()
+export const usersRepository =
+  env.NODE_ENV === 'production'
+    ? new PrismaUsersRepository()
+    : new InMemoryUsersRepository()

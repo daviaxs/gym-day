9442a13c8f59de0677802db269ed4fe6551ea220
Davi Alves <cadwvk@gmail.com>
2023-12-30 11:23:03 -0300

    feat: Add InMemoryUsersRepository implementation

---
+++ apps/back-end/src/repositories/in-memory/in-memory-users-repository.ts
@@ -0,0 +1,43 @@
+import { Prisma, User } from '@prisma/client'
+import { UsersRepository } from '../users-repository'
+import { randomUUID } from 'node:crypto'
+
+export class InMemoryUsersRepository implements UsersRepository {
+  public users: User[] = []
+
+  async findUserById(id: string) {
+    const user = this.users.find((user) => user.id === id)
+
+    if (!user) {
+      return null
+    }
+
+    return user
+  }
+
+  async findUserByEmail(email: string) {
+    const user = this.users.find((user) => user.email === email)
+
+    if (!user) {
+      return null
+    }
+
+    return user
+  }
+
+  async createUser(data: Prisma.UserCreateInput) {
+    const user = {
+      id: randomUUID(),
+      name: data.name,
+      email: data.email,
+      password_hash: data.password_hash,
+      created_at: new Date(),
+      updated_at: new Date(),
+      role: data.role ?? 'MEMBER',
+    }
+
+    this.users.push(user)
+
+    return user
+  }
+}

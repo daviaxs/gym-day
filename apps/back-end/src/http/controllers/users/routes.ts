5f33d7f3888b7d38992b1eaafea4bc3385035538
Davi Alves <cadwvk@gmail.com>
2023-12-30 15:30:06 -0300

    feat: Add user authentication functionality

--- apps/back-end/src/http/controllers/users/routes.ts
+++ apps/back-end/src/http/controllers/users/routes.ts
@@ -2,9 +2,11 @@ import { FastifyInstance } from 'fastify'
 import { register } from './register'
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
 import { profile } from './profile'
+import { authenticate } from './authenticate'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
+  app.post('/authenticate', authenticate)
 
   app.get('/me', { onRequest: [verifyJWT] }, profile)
 }

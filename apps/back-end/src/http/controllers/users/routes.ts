cc669b28c81da8bfe310006a7a4d2743052fa5df
Davi Alves <cadwvk@gmail.com>
2023-12-30 16:32:46 -0300

    feat: Add refresh token functionality

--- apps/back-end/src/http/controllers/users/routes.ts
+++ apps/back-end/src/http/controllers/users/routes.ts
@@ -3,10 +3,12 @@ import { register } from './register'
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
 import { profile } from './profile'
 import { authenticate } from './authenticate'
+import { refresh } from './refresh'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/authenticate', authenticate)
+  app.patch('/token/refresh', refresh)
 
   app.get('/me', { onRequest: [verifyJWT] }, profile)
 }

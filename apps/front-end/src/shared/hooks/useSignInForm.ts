24c6704b237bd057a36bfc7fa69d0cec3419f11f
Davi Alves <cadwvk@gmail.com>
2024-01-03 19:35:45 -0300

    feat: Add error handling and API call to sign in form

--- apps/front-end/src/shared/hooks/useSignInForm.ts
+++ apps/front-end/src/shared/hooks/useSignInForm.ts
@@ -1,18 +1,43 @@
 import { useForm } from 'react-hook-form'
 import { signInUserData, signIpSchema } from '../schemas/SignInSchema'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { api } from '@/lib/axios'
+import { setCookie } from 'nookies'
+import { useState } from 'react'
 
 export function useSignInForm() {
+  const [errorMessage, setErrorMessage] = useState<string | null>()
+
   const methods = useForm<signInUserData>({
     resolver: zodResolver(signIpSchema),
     mode: 'onChange',
   })
 
   const signIn = (data: signInUserData) => {
+    const { email, password } = data
+
+    api
+      .post('/authenticate', {
+        email,
+        password,
+      })
+      .then((response) => {
+        setErrorMessage(null)
+        setCookie(undefined, 'GymDay.accessToken', response.data.token, {
+          maxAge: 60 * 60 * 24 * 7, // 7 days
+          path: '/',
+        })
+        window.location.href = '/app/inicio'
+      })
+      .catch((e) => {
+        setErrorMessage(e.response.data.message)
+      })
+
     console.log(data)
   }
 
   return {
+    errorMessage,
     signIn,
     methods,
   }

582ce2b23830dde5f09b2bc1de18ea990aa68b63
Davi Alves <cadwvk@gmail.com>
2024-01-03 14:12:07 -0300

    feat: Add packages-gym-day-rules dependency and use name validation rule in back-end

--- apps/back-end/src/http/controllers/users/register.ts
+++ apps/back-end/src/http/controllers/users/register.ts
@@ -1,27 +1,11 @@
 import { makeRegisterUseCase } from '@/use-cases/factories/make-register-use-case'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { ZodError, z } from 'zod'
+import { rules } from 'packages-gym-day-rules/rules'
 
 export async function register(req: FastifyRequest, reply: FastifyReply) {
   const registerBodySChema = z.object({
-    name: z
-      .string()
-      .min(3, { message: 'O nome precisa ter no mínimo 3 caracteres' })
-      .max(20, { message: 'O nome precisa ter no máximo 20 caracteres' })
-      .transform((name) => {
-        return name
-          .trim()
-          .split(' ')
-          .map((word) => {
-            return word[0]
-              .toLocaleUpperCase()
-              .concat(word.substring(1).toLocaleLowerCase())
-          })
-          .join(' ')
-      })
-      .refine((value) => /^[A-Za-zÀ-ÖØ-öø-ÿ\s]*$/.test(value), {
-        message: 'O nome não pode conter números ou caracteres especiais',
-      }),
+    name: rules.nameValidation,
 
     email: z.string().email(),
 

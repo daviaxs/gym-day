bb8cf4709ee091a6275b6c7a037e0a14092c96a3
Davi Alves <cadwvk@gmail.com>
2024-01-03 14:12:20 -0300

    Add packages-gym-day-rules dependency and update name validation in signUpSchema in front-end

--- apps/front-end/src/shared/schemas/signUpSchema.ts
+++ apps/front-end/src/shared/schemas/signUpSchema.ts
@@ -1,25 +1,9 @@
 import { z } from 'zod'
+import { rules } from 'packages-gym-day-rules/rules'
 
 export const signUpSchema = z
   .object({
-    name: z
-      .string()
-      .min(3, { message: 'O nome precisa ter no mínimo 3 caracteres' })
-      .max(20, { message: 'O nome precisa ter no máximo 20 caracteres' })
-      .transform((name) => {
-        return name
-          .trim()
-          .split(' ')
-          .map((word) => {
-            return word[0]
-              .toLocaleUpperCase()
-              .concat(word.substring(1).toLocaleLowerCase())
-          })
-          .join(' ')
-      })
-      .refine((value) => /^[A-Za-zÀ-ÖØ-öø-ÿ\s]*$/.test(value), {
-        message: 'O nome não pode conter números ou caracteres especiais',
-      }),
+    name: rules.nameValidation,
 
     email: z.string().email('E-mail inválido'),
 

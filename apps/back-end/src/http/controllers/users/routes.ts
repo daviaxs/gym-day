b2227872e4b80b9539ccb581dcc5bdf6166421ab
Davi Alves <cadwvk@gmail.com>
2024-01-03 19:34:27 -0300

    feat: Add verifyToken route for user authentication

--- apps/back-end/src/http/controllers/users/routes.ts
+++ apps/back-end/src/http/controllers/users/routes.ts
@@ -5,11 +5,13 @@ import { profile } from './profile'
 import { authenticate } from './authenticate'
 import { refresh } from './refresh'
 import { deleteUser } from './delete'
+import { verifyToken } from './verifyToken'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/authenticate', authenticate)
   app.patch('/token/refresh', refresh)
+  app.get('/verifyToken', verifyToken)
 
   app.get('/me', { onRequest: [verifyJWT] }, profile)
   app.delete('/me', { onRequest: [verifyJWT] }, deleteUser)

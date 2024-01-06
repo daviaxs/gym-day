5f7509084022d2795fe8e9d44c989eff45f22734
Davi Alves <cadwvk@gmail.com>
2024-01-06 19:24:56 -0300

    Remove unused imports

--- apps/front-end/src/app/app/inicio/utils/UserFirstName.tsx
+++ apps/front-end/src/app/app/inicio/utils/UserFirstName.tsx
@@ -1,8 +1,6 @@
 'use client'
 
-import { Text } from '@/shared/components/text/Text'
 import { useGetUserValues } from '@/shared/hooks/useGetUserValues'
-import { inter } from '@/shared/styles/theme/fonts'
 
 export function UserFisrtName() {
   const { userValues } = useGetUserValues()

1f925dc1a5ebd46e59fa51549b9ebb7a009d64c9
Davi Alves <cadwvk@gmail.com>
2024-01-07 12:54:02 -0300

    feat: Add SpanContainer and FirstInteractionMessage components to Home page

--- apps/front-end/src/app/app/inicio/page.tsx
+++ apps/front-end/src/app/app/inicio/page.tsx
@@ -2,13 +2,19 @@ import { Text } from '@/shared/components/text/Text'
 import { UserFisrtName } from './utils/UserFirstName'
 import { inter } from '@/shared/styles/theme/fonts'
 import { HomeStyle } from './Home.style'
+import { SpanContainer } from '@/shared/components/spanContainer/SpanContainer.style'
+import { FirstInterationMessage } from './utils/FirstInteractionMessage'
 
 export default function Home() {
   return (
     <HomeStyle>
-      <Text className={`${inter.className}`} size="3xl" $weight="800">
-        Olá, <UserFisrtName />
-      </Text>
+      <SpanContainer $align="center" $gap={1}>
+        <Text className={`${inter.className}`} size="3xl" $weight="800">
+          Olá, <UserFisrtName />
+        </Text>
+
+        <FirstInterationMessage />
+      </SpanContainer>
     </HomeStyle>
   )
 }

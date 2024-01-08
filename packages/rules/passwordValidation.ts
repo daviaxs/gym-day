9981fa987f57fb1f814361a29c7d95ab967ff396
Davi Alves <cadwvk@gmail.com>
2024-01-08 19:33:20 -0300

    feat: Update passwordValidation rule to include maximum character limit

--- packages/rules/passwordValidation.ts
+++ packages/rules/passwordValidation.ts
@@ -1,3 +1,3 @@
 import { z } from "zod";
 
-export const passwordValidation = z.string().min(6, 'A senha precisa de no mínimo 6 caracteres')
+export const passwordValidation = z.string().min(6, 'A senha precisa de no mínimo 6 caracteres').max(30, 'Limite de 30 caracteres excedido')

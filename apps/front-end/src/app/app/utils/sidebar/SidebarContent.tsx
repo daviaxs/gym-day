d9e977ba56d442639b15ee2a79112cd4c0d519c0
Davi Alves <cadwvk@gmail.com>
2024-01-10 16:02:29 -0300

    feat: Add navigation buttons to sidebar

--- apps/front-end/src/app/app/utils/sidebar/SidebarContent.tsx
+++ apps/front-end/src/app/app/utils/sidebar/SidebarContent.tsx
@@ -1,5 +1,15 @@
 import { SidebarContentStyle } from './Sidebar.style'
+import { NavButton } from './utils/navButton/NavButton'
+import { NavLinks } from './utils/navLinks'
 
 export function SidebarContent() {
-  return <SidebarContentStyle></SidebarContentStyle>
+  return (
+    <SidebarContentStyle>
+      {NavLinks.map((link, index) => (
+        <NavButton key={index} to={link.to} iconName={link.icon}>
+          {link.children}
+        </NavButton>
+      ))}
+    </SidebarContentStyle>
+  )
 }

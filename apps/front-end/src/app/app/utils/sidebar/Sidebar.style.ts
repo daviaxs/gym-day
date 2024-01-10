d55d8012082bf5af36e3a5e9c0cffe95bdf4a886
Davi Alves <cadwvk@gmail.com>
2024-01-10 12:19:57 -0300

    css: Add styling to Sidebar component

--- apps/front-end/src/app/app/utils/sidebar/Sidebar.style.ts
+++ apps/front-end/src/app/app/utils/sidebar/Sidebar.style.ts
@@ -2,4 +2,17 @@
 
 import styled from 'styled-components'
 
-export const SidebarContainer = styled.aside``
+export const SidebarContainer = styled.aside`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: start;
+
+  gap: 2rem;
+
+  height: 100vh;
+  width: 15rem;
+
+  border-right: 1px solid #333b5c70;
+  background: linear-gradient(220deg, #353a69 -9.86%, #03000c 90.92%);
+`

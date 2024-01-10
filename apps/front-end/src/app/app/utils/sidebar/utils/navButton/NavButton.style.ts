48581dc10c4060f37d40605679f6c7e87465172a
Davi Alves <cadwvk@gmail.com>
2024-01-10 20:01:47 -0300

    css: Add styling changes to NavButton component

--- apps/front-end/src/app/app/utils/sidebar/utils/navButton/NavButton.style.ts
+++ apps/front-end/src/app/app/utils/sidebar/utils/navButton/NavButton.style.ts
@@ -15,13 +15,25 @@ export const NavButtonStyle = styled.button<NavButtonProps>`
   width: 100%;
   height: 3rem;
   padding: 0 1rem;
+  gap: 1rem;
 
   border-radius: 0.375rem;
   border: 1px solid rgba(138, 142, 161, 0.2);
   background-color: ${({ $isActive }) =>
     $isActive ? 'rgba(176, 179, 192, 0.2)' : 'transparent'};
 
+  transition: all 0.2s ease-in-out;
+
+  .icon {
+    transition: all 0.2s ease-in-out;
+    ${({ $isActive }) => !$isActive && 'opacity: 0.5;'}
+  }
+
   &:hover {
     background: rgba(176, 179, 192, 0.2);
+
+    .icon {
+      transform: scale(1.2);
+    }
   }
 `

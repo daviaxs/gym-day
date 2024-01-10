2397e650cb2df9b427b4b86c33908a6e63bccc13
Davi Alves <cadwvk@gmail.com>
2024-01-10 15:00:21 -0300

    feat: Update sidebarHeader styles and add logo to sidebar header

--- apps/front-end/src/app/app/utils/sidebar/Sidebar.style.ts
+++ apps/front-end/src/app/app/utils/sidebar/Sidebar.style.ts
@@ -5,8 +5,8 @@ import styled from 'styled-components'
 export const SidebarContainer = styled.aside`
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: start;
+  justify-content: start;
+  align-items: center;
 
   gap: 2rem;
 
@@ -16,7 +16,21 @@ export const SidebarContainer = styled.aside`
   border-right: 1px solid #333b5c70;
   background: linear-gradient(220deg, #353a69 -9.86%, #03000c 90.92%);
 `
-export const SidebarHeaderStyle = styled.div``
+export const SidebarHeaderStyle = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  width: 100%;
+  height: fit-content;
+  padding: 0.8rem 0.5rem;
+
+  border-bottom: 2px solid rgba(84, 91, 118, 0.2);
+
+  .gym-day-logo {
+    width: 120px;
+  }
+`
 
 export const SidebarContentStyle = styled.nav``
 
--- apps/front-end/src/app/app/utils/sidebar/SidebarHeader.tsx
+++ apps/front-end/src/app/app/utils/sidebar/SidebarHeader.tsx
@@ -1,5 +1,13 @@
+import { GymDayLogoPurpleSmall } from '@/shared/assets/gym-day-logos/gym-day-logo-purple-small'
 import { SidebarHeaderStyle } from './Sidebar.style'
+import { ArrowLeft } from 'lucide-react'
+import { themeColors } from '@/shared/styles/theme/palette'
 
 export function SidebarHeader() {
-  return <SidebarHeaderStyle></SidebarHeaderStyle>
+  return (
+    <SidebarHeaderStyle>
+      <GymDayLogoPurpleSmall />
+      <ArrowLeft size={30} color={themeColors['purple-50']} />
+    </SidebarHeaderStyle>
+  )
 }

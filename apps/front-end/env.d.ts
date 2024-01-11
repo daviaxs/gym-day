b366b4d3af35f8293c4fb30f3788a2af09e2e20f
Davi Alves <cadwvk@gmail.com>
2024-01-11 14:57:08 -0300

    ref: Update NODE_ENV type in env.d.ts

--- apps/front-end/env.d.ts
+++ apps/front-end/env.d.ts
@@ -1,6 +1,6 @@
 declare namespace NodeJS {
   interface ProcessEnv {
-    NODE_ENV: string
+    NODE_ENV: 'dev' | 'prod'
     NEXT_PUBLIC_RECAPTCHA_SITE_KEY: string
     RECAPTCHA_SECRET_KEY: string
     NEXT_PUBLIC_API_BASE_URL: string

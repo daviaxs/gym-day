4155306ae064558b67fe3f73b6b205ecfcd4514e
Davi Alves <cadwvk@gmail.com>
2024-01-15 15:21:02 -0300

    feat: Add logout functionality in handleLogout function

--- apps/front-end/src/shared/components/settingsMenu/funcs/handleLogout.ts
+++ apps/front-end/src/shared/components/settingsMenu/funcs/handleLogout.ts
@@ -1,6 +1,9 @@
+import { api } from '@/lib/axios'
 import { GYM_DAY_ACCESS_TOKEN } from '@/shared/constants/cookiesNames'
 
-export function handleLogout() {
+export async function handleLogout() {
+  await api.post('/logout')
+
   document.cookie = `${GYM_DAY_ACCESS_TOKEN}=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT`
   window.location.href = '/'
 }

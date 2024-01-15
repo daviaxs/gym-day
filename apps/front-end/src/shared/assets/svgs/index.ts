6fe933e44f4e55e31b87fb2484306ef248dd887b
Davi Alves <cadwvk@gmail.com>
2024-01-15 13:40:16 -0300

    feat: Add Alert SVG icon

--- apps/front-end/src/shared/assets/svgs/index.ts
+++ apps/front-end/src/shared/assets/svgs/index.ts
@@ -2,5 +2,6 @@ import { Constancy } from './Constancy'
 import { Discipline } from './Discipline'
 import { Focus } from './Focus'
 import { HappyFace } from './HappyFace'
+import { Alert } from './Alert'
 
-export { Constancy, Discipline, Focus, HappyFace }
+export { Constancy, Discipline, Focus, HappyFace, Alert }
